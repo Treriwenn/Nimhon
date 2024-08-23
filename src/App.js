0feb46efab209a79a3eebbc6fcdee2b86209c3ef
Treriwen <paul21fe@gmail.com>
2024-08-23 19:44:49 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import "./styles/app.scss";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
+import Home from './pages/home/Home'
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/accueil" />} />
+        <Route path="/accueil" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/logement/:id" element={<FichLogement />} />
+        <Route path="*" element={<NoPage />} /> */}
+      </Routes>
+    </HashRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import Banner from "../../components/banner/Banner";
+// import Footer from "../../components/footer/Footer";
+// import Gallery from "../../components/gallery/Gallery";
+// import Header from "../../components/header/Header";
+// import imgHomeBanner from "../../assets/images/banner/homeBanner.png";
+
+const Home = () => {
+  return (
+    <div className="home">
+      {/* <Header /> */}
+      <main>
+        {/* <Gallery /> */}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default Home;
